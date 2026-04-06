caa564012dd57ebfd7f9ddfea1b866e0e82428c6
massacring <abbenylund@gmail.com>
2026-04-06 13:38:00 +0200

    Another final test.

--- Libraries/Installer.lua
+++ Libraries/Installer.lua
@@ -1,13 +1,14 @@
 -- Meant to be run with wget in a computer or turtle terminal, not installed.
 -- Usage: wget run https://raw.githubusercontent.com/massacring/CC-Tweaked/main/Libraries/Installer.lua <args>
 
-local args = {}
+local args = arg
+local arg = {}
 local counter = 0
-for k,v in pairs(arg) do
+for k,v in pairs(args) do
     counter = counter + 1
     if counter > 2 and k ~= 0 then
-        table.insert(args, v)
+        table.insert(arg, v)
     end
 end
 
-print(textutils.serialize(args))
+print(textutils.serialize(arg))

94be2f069fe7ae87b94642c781b2b2c9531de987
massacring <abbenylund@gmail.com>
2026-04-06 13:42:41 +0200

    Another print move and added pre-print

--- Libraries/Installer.lua
+++ Libraries/Installer.lua
@@ -1,13 +1,15 @@
 -- Meant to be run with wget in a computer or turtle terminal, not installed.
 -- Usage: wget run https://raw.githubusercontent.com/massacring/CC-Tweaked/main/Libraries/Installer.lua <args>
 
+print(textutils.serialize(arg))
 local args = arg
 local arg = {}
 local counter = 0
 for k,v in pairs(args) do
     counter = counter + 1
     if counter > 2 and k ~= 0 then
-        print(textutils.serialize(arg))
         table.insert(arg, v)
     end
 end
+
+print(textutils.serialize(arg))

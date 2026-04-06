a10c259bac2a31339bcfe545de21e8ad54f89d51
massacring <abbenylund@gmail.com>
2026-04-06 13:18:44 +0200

    Further wget testing.

--- Libraries/Installer.lua
+++ Libraries/Installer.lua
@@ -1,1 +1,10 @@
-print(textutils.serialize(arg))
+-- Meant to be run with wget in a computer or turtle terminal, not installed.
+-- Usage: wget run https://raw.githubusercontent.com/massacring/CC-Tweaked/main/Libraries/Installer.lua <args>
+
+local args = {}
+for k,v in pairs(arg) do
+    args[k] = v
+end
+args[0] = nil
+
+print(textutils.serialize(args))

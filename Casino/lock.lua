5b567aebec1e1819dbf9049fcd7a501941a6a722
massacring <abbenylund@gmail.com>
2026-04-07 23:02:49 +0200

    Added support for toggling the lock with redstone input.

--- Casino/lock.lua
+++ Casino/lock.lua
@@ -1,19 +1,41 @@
 os.pullEvent = os.pullEventRaw
 
-local direction = "back"
+local outputDirection = "back"
+local inputDirection = "top"
 
-redstone.setOutput(direction, true)
+redstone.setOutput(outputDirection, true)
 
 local lock = false
 
 local timerTime = 15 * 60
 local timerID
 
+local function runRedstone()
+    while true do
+        os.pullEvent("redstone")
+        if redstone.getInput(inputDirection) then
+            lock = not lock
+            if timerID ~= nil then
+                os.cancelTimer(timerID)
+            end
+            if lock then
+                timerID = os.startTimer(timerTime)
+            end
+            redstone.setOutput(outputDirection, not lock)
+            sleep(1)
+        end
+    end
+end
+
 local function runTimer()
     local _, id = os.pullEvent("timer")
     if id == timerID then
         os.reboot()
     end
+end
+
+local function runEvents()
+    parallel.waitForAll(runRedstone, runTimer)
 end
 
 local function runCommands()
@@ -25,7 +47,9 @@ local function runCommands()
         write(" - lock (lasts 15 minutes)\n")
         write(" - unlock\n")
         write("> ")
+
         local input = read()
+
         if input == "lock" then
             lock = true
             if timerID ~= nil then
@@ -38,8 +62,9 @@ local function runCommands()
                 os.cancelTimer(timerID)
             end
         end
-        redstone.setOutput(direction, not lock)
+        redstone.setOutput(outputDirection, not lock)
     end
 end
 
-parallel.waitForAll(runTimer, runCommands)
+parallel.waitForAll(runEvents, runCommands)
+
